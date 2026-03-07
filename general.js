b280f6ce82ef6bceb09a4747a0e0eea74341c977
Ndemogbe Juana <22ndemogbej@gmail.com>
2026-03-07 21:45:28 +0200

    message

---
+++ general.js
@@ -0,0 +1,63 @@
+function genWally() {
+    
+    let fromTop = Math.random() * document.body.scrollHeight;
+    let fromLeft = Math.random() * document.body.scrollWidth;
+
+    let wally = document.createElement("img");
+    wally.src = "Images/wally.png";
+    wally.id = "wally";
+
+    wally.style.position = "absolute";
+    wally.style.width = "2.5%";
+    wally.style.opacity = "50%"
+    wally.style.left = fromLeft + "px";
+    wally.style.top = fromTop + "px";
+    wally.style.zIndex = 5;
+
+    document.body.appendChild(wally);
+    
+
+}
+
+function clickWally(){
+    let wally = document.getElementById("wally");
+    let left = wally.style.left;
+    let top = wally.style.top;
+    
+    // Create the message
+    let text = document.createElement("p");
+    text.style.position = "absolute";
+    text.style.left = left;
+    text.style.top = top;
+    text.style.fontSize = "50px";
+    text.style.color = "red";
+    text.style.zIndex = 5;
+    
+    // Start fully opaque
+    text.style.opacity = 1;
+    // Add a smooth fade effect
+    text.style.transition = "opacity 2s ease";
+
+    text.innerHTML = "You found Wally!!!";
+
+    // Remove Wally
+    document.body.removeChild(wally);
+    // Add message to the page
+    document.body.appendChild(text);
+
+    // Trigger fade after a tiny delay
+    setTimeout(() => {
+        text.style.opacity = 0;
+    }, 50); // small delay ensures transition works
+
+    // Remove message from DOM after fade
+    //text.parentNode.removeChild(text);
+
+}
+
+window.onload = genWally;
+document.addEventListener("click", clickWally);
+
+// function clickWally(){
+
+// }
